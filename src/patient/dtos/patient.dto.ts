2bc3753253c710b98d840bbd1cc7bf3252c55a37
geperezz <gph393@gmail.com>
2024-06-15 19:44:44 -0400

    Check that the birthdate comes before today

--- src/patient/dtos/patient.dto.ts
+++ src/patient/dtos/patient.dto.ts
@@ -7,7 +7,12 @@ import { userDtoSchema } from 'src/user/dtos/user.dto';
 import { treatmentDtoSchema } from 'src/treatment/dtos/treatment.dto';
 
 export const patientDtoSchema = userDtoSchema.omit({ role: true }).extend({
-  birthdate: z.coerce.date().nullable(),
+  birthdate: z.coerce
+    .date()
+    .nullable()
+    .refine(
+      (birthdate) => !birthdate || birthdate.valueOf() <= Date.now().valueOf(),
+    ),
   age: z.coerce.number().int().min(0).nullable(),
   weightInKg: z.coerce.number().gt(0).nullable(),
   heightInCm: z.coerce.number().min(1).nullable(),
