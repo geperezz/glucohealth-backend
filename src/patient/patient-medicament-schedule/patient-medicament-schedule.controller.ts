db5bea631f601276ccfa8b16f230561ebf0c9fbd
geperezz <gph393@gmail.com>
2024-06-17 15:28:30 -0400

    Add route for patients to retrieve their schedules

--- src/patient/patient-medicament-schedule/patient-medicament-schedule.controller.ts
+++ src/patient/patient-medicament-schedule/patient-medicament-schedule.controller.ts
@@ -7,15 +7,17 @@ import { patientDtoSchema } from 'src/patient/dtos/patient.dto';
 import { PatientMedicamentScheduleDto } from './dtos/patient-medicament-schedule.dto';
 import { PatientMedicamentScheduleService } from './patient-medicament-schedule.service';
 import { MustBeLoggedInAs } from 'src/auth/must-be-logged-in-as.decorator';
+import { UserFromPayload } from 'src/auth/user-from-payload.decorator';
+import { User } from 'src/user/user.repository';
 
-@Controller('/patients/:patientId/treatment/schedules/')
+@Controller()
 @ApiTags("Schedules for the patients' treatments")
 export class PatientMedicamentScheduleController {
   constructor(
     private readonly patientMedicamentScheduleService: PatientMedicamentScheduleService,
   ) {}
 
-  @Get('/:scheduleDate/')
+  @Get('/patients/:patientId/treatment/schedules/:scheduleDate/')
   @MustBeLoggedInAs('admin', 'nurse')
   async findOne(
     @Param('patientId', new ValidationPipe(patientDtoSchema.shape.id))
@@ -25,6 +27,24 @@ export class PatientMedicamentScheduleController {
   ): Promise<PatientMedicamentScheduleDto[]> {
     const schedule = await this.patientMedicamentScheduleService.findOne(
       patientId,
+      scheduleDate,
+    );
+
+    return schedule.map((schedule) =>
+      PatientMedicamentScheduleDto.fromSchema(schedule),
+    );
+  }
+
+  @Get('/patients/me/treatment/schedules/:scheduleDate/')
+  @MustBeLoggedInAs('patient')
+  async findMine(
+    @UserFromPayload()
+    me: User,
+    @Param('scheduleDate', new ValidationPipe(z.coerce.date()))
+    scheduleDate: Date,
+  ): Promise<PatientMedicamentScheduleDto[]> {
+    const schedule = await this.patientMedicamentScheduleService.findOne(
+      me.id,
       scheduleDate,
     );
 
